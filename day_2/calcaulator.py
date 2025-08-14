e31186ba786f6bde8bd71343ab843fac9a41bc57
rics254 <42621460+rics254@users.noreply.github.com>
2025-08-15 00:43:32 +0300

    Day 2 calculator project

---
+++ day_2/calcaulator.py
@@ -0,0 +1,27 @@
+#calculator.py
+
+print("Simple Calculator")
+print("Operations: +  -  *  /")
+
+# Get input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+
+# Process
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Division by zero."
+else:
+    result = "Invalid operation"
+
+# Output
+print("Result:", result)
