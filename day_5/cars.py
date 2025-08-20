574e019823ab6825c10e1776b28485e9a163a7ee
rics254 <42621460+rics254@users.noreply.github.com>
2025-08-21 00:55:28 +0300

    today was all about sorting lists

---
+++ day_5/cars.py
@@ -0,0 +1,27 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+#sorting in resverse order
+cars.sort(reverse=True)
+print(cars)
+
+#sorting temporarily
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("Here is the original list of the cars: ")
+print(cars)
+print("\nhere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again: ")
+print(cars)
+
+#printing a list in reverse order.
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars.reverse()
+print(cars)
+
+#finding the length of a list.
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
---
+++ day_5/location.py
@@ -0,0 +1,18 @@
+location = ['japan', 'china', 'israel','hawaii','coast']
+print("\nThis is the original ,order of the list")
+print(location)
+
+#sorting temporarily
+print("\nThis is the sorted list of location")
+print(sorted(location))
+
+print("\nThis is the original list")
+print(location)
+
+#sorting in reverve mode
+print(sorted(location, reverse=True))
+
+print(location)
+
+print(location.reverse())
+print(location)
